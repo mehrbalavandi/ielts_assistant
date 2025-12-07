ae77810532332f3b93db7dd4b547c534b896c16f
mehrbalavandi <mehrbalavandi@gmail.com>
2025-12-07 14:32:53 +0330

    #

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,17 @@
+import java.util.Properties
+import java.io.FileInputStream
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+// ✅
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -29,14 +38,44 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+    // ✅
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // 🧱        
+            // signingConfig = signingConfigs.getByName("debug")
+            // 🧪        
+            signingConfig = signingConfigs.getByName("release")
+            // ✅
+            isMinifyEnabled = true
+            isShrinkResources = true
+            // 🧱 
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )  
+            // 🧪
+            //proguardFiles("proguard-rules.pro")  
         }
     }
+}
+
+// ✅
+dependencies {
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    
+// سایر وابستگی‌ها...
+  implementation("com.google.android.play:core:1.10.3")
 }
 
 flutter {
