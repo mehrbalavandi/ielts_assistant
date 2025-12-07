8a698e2c9ec5f11461a6ec609d90c896af292237
mehrbalavandi <mehrbalavandi@gmail.com>
2025-12-07 15:44:58 +0330

    #

--- lib/common/cf_public.dart
+++ lib/common/cf_public.dart
@@ -1,66 +1,66 @@
-import 'dart:convert';
-import 'dart:io';
+// import 'dart:convert';
+// import 'dart:io';
 
-// import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:file_selector/file_selector.dart';
-import 'package:path_provider/path_provider.dart' show getTemporaryDirectory;
-import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
+// // import 'package:awesome_notifications/awesome_notifications.dart';
+// import 'package:file_selector/file_selector.dart';
+// import 'package:path_provider/path_provider.dart' show getTemporaryDirectory;
+// import 'package:flutter/material.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
-Future<bool> setImagesPathNewMethod(SettingsController settingsCtrl) async {
-  try {
-    var res = await Permission.manageExternalStorage.status;
-    if (!res.isGranted) {
-      Permission.manageExternalStorage.request().then((onValue) async {
-        var res2 = await Permission.manageExternalStorage.status;
-        if (res2.isGranted) {
-          try {
-            String? previousPath;
-            if (await Directory(imagesPath).exists()) {
-              previousPath = imagesPath;
-            }
-            String? selectedDirectory = (previousPath != null)
-                ? await getDirectoryPath(initialDirectory: previousPath)
-                : await getDirectoryPath();
+// Future<bool> setImagesPathNewMethod(SettingsController settingsCtrl) async {
+//   try {
+//     var res = await Permission.manageExternalStorage.status;
+//     if (!res.isGranted) {
+//       Permission.manageExternalStorage.request().then((onValue) async {
+//         var res2 = await Permission.manageExternalStorage.status;
+//         if (res2.isGranted) {
+//           try {
+//             String? previousPath;
+//             if (await Directory(imagesPath).exists()) {
+//               previousPath = imagesPath;
+//             }
+//             String? selectedDirectory = (previousPath != null)
+//                 ? await getDirectoryPath(initialDirectory: previousPath)
+//                 : await getDirectoryPath();
 
-            if (selectedDirectory != null) {
-              await box.write(
-                StorageVariables.imagesPath.name,
-                selectedDirectory,
-              );
-              if (selectedDirectory != imagesPath) {
-                stuffTypesListProvider.notifyToListeners(flag: true);
-                imagesPath = selectedDirectory;
-                settingsCtrl.updateImagesPath(selectedDirectory);
-                return true;
-              }
-            }
-          } catch (exception) {
-            // TODO
-          }
-        }
-      });
-    } else if (res.isGranted) {
-      try {
-        String? previousPath;
-        if (await Directory(imagesPath).exists()) {
-          previousPath = imagesPath;
-        }
-        String? selectedDirectory = (previousPath != null)
-            ? await getDirectoryPath(initialDirectory: previousPath)
-            : await getDirectoryPath();
+//             if (selectedDirectory != null) {
+//               await box.write(
+//                 StorageVariables.imagesPath.name,
+//                 selectedDirectory,
+//               );
+//               if (selectedDirectory != imagesPath) {
+//                 stuffTypesListProvider.notifyToListeners(flag: true);
+//                 imagesPath = selectedDirectory;
+//                 settingsCtrl.updateImagesPath(selectedDirectory);
+//                 return true;
+//               }
+//             }
+//           } catch (exception) {
+//             // TODO
+//           }
+//         }
+//       });
+//     } else if (res.isGranted) {
+//       try {
+//         String? previousPath;
+//         if (await Directory(imagesPath).exists()) {
+//           previousPath = imagesPath;
+//         }
+//         String? selectedDirectory = (previousPath != null)
+//             ? await getDirectoryPath(initialDirectory: previousPath)
+//             : await getDirectoryPath();
 
-        if (selectedDirectory != null) {
-          await box.write(StorageVariables.imagesPath.name, selectedDirectory);
-          if (selectedDirectory != imagesPath) {
-            stuffTypesListProvider.notifyToListeners(flag: true);
-            imagesPath = selectedDirectory;
-            settingsCtrl.updateImagesPath(selectedDirectory);
-            return true;
-          }
-        }
-      } catch (exception) {}
-    }
-  } catch (exception) {}
-  return false;
-}
+//         if (selectedDirectory != null) {
+//           await box.write(StorageVariables.imagesPath.name, selectedDirectory);
+//           if (selectedDirectory != imagesPath) {
+//             stuffTypesListProvider.notifyToListeners(flag: true);
+//             imagesPath = selectedDirectory;
+//             settingsCtrl.updateImagesPath(selectedDirectory);
+//             return true;
+//           }
+//         }
+//       } catch (exception) {}
+//     }
+//   } catch (exception) {}
+//   return false;
+// }
