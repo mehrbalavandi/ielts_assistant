1a85207dd516eab9f964d94988a8969bf27c4114
mehrbalavandi <mehrbalavandi@gmail.com>
2026-01-06 16:06:17 +0330

    #

--- lib/features/content_viewer/presentation/topic_detail_screen.dart
+++ lib/features/content_viewer/presentation/topic_detail_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:ielts_assistant/common/enums.dart';
 import 'package:ielts_assistant/features/audio_player/presentation/widgets/expandable_mini_player.dart';
 import 'package:ielts_assistant/features/content_viewer/data/models.dart';
 import 'package:ielts_assistant/shared/models/content_models.dart';
@@ -63,15 +65,18 @@ class _TopicDetailScreenState extends ConsumerState<TopicDetailScreen> {
   Widget build(BuildContext context) {
     final nav = ref.watch(navigationProvider);
     final isDualPane = ref.watch(isDualPaneProvider);
-    final topic = nav.selectedTopic;
+    final selectedTopic = nav.selectedTopic;
+    final mainTextSegments = nav.currentEnglishSegments ?? <MainTextSegment>[];
+    final persianTextSegments =
+        nav.currentPersianTextSegments ?? <PersianTextSegment>[];
 
-    if (topic == null) {
+    if (selectedTopic == null) {
       return const Scaffold(body: Center(child: Text('درسی انتخاب نشده است')));
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(topic.name),
+        title: Text(selectedTopic.name),
         actions: [
           IconButton(
             icon: Icon(isDualPane ? Icons.view_stream : Icons.view_column),
@@ -83,7 +88,14 @@ class _TopicDetailScreenState extends ConsumerState<TopicDetailScreen> {
       ),
       body: Column(
         children: [
-          Expanded(child: isDualPane ? _buildDualPane() : _buildSinglePane()),
+          Expanded(
+            child: isDualPane
+                ? _buildBothEnglishAndPersianLayout(
+                    mainTextSegments,
+                    persianTextSegments,
+                  )
+                : _buildOnlyEnglishLayout(mainTextSegments),
+          ),
           // مینی پلیر که قابلیت باز و بسته شدن دارد
           const ExpandableMiniPlayer(),
         ],
@@ -92,42 +104,110 @@ class _TopicDetailScreenState extends ConsumerState<TopicDetailScreen> {
   }
 
   // چیدمان تک ستونه (انگلیسی بالای فارسی)
-  Widget _buildSinglePane() {
-    return ListView(
-      controller: _englishController, // استفاده از کنترلر مشترک برای حفظ موقعیت
-      padding: const EdgeInsets.all(16),
-      children: [
-        _buildEnglishSection(),
-        const Divider(height: 40),
-        _buildPersianSection(),
-      ],
+  Widget _buildOnlyEnglishLayout(List<MainTextSegment> mainTexSegments) {
+    int interactiveIndex = 0;
+    final List<InlineSpan> spans = mainTexSegments.map((item) {
+      if (item.isInteractive) {
+        return TextSpan(
+          text: '(${++interactiveIndex})${item.text}'.replaceAll(
+            '\\n',
+            '\n',
+          ), // اعمال استایل بر اساس status
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.error,
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
+          recognizer: TapGestureRecognizer()
+            ..onTap = () {
+              _showPopup(
+                context,
+                item.text,
+                item.translation!,
+                item.explanation!,
+              );
+            },
+        );
+      } else {
+        return TextSpan(
+          text: item.text.replaceAll(
+            '\\n',
+            '\n',
+          ), // اعمال استایل بر اساس status
+          style: (item.isBold != null && item.isBold == true)
+              ? TextStyle(
+                  backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  fontWeight: FontWeight.bold,
+                  // fontStyle: FontStyle.italic,
+                  fontSize: 20.0,
+                )
+              : TextStyle(
+                  color: Theme.of(context).colorScheme.onSurface,
+                  fontSize: 20.0,
+                ),
+        );
+      }
+    }).toList();
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: RichText(
+          textAlign: TextAlign.left,
+          text: TextSpan(children: spans),
+        ),
+      ),
     );
   }
 
   // چیدمان دو ستونه (دو لیست اسکرول‌شونده مجزا)
-  Widget _buildDualPane() {
-    return Row(
-      children: [
-        Expanded(
-          child: ListView(
-            controller: _englishController,
-            padding: const EdgeInsets.all(16),
-            children: [_buildEnglishSection()],
-          ),
+  Widget _buildBothEnglishAndPersianLayout(
+    List<MainTextSegment> mainTexSegments,
+    List<PersianTextSegment> translationTextSegments,
+  ) {
+    final List<TextSpan> spans = translationTextSegments.map((item) {
+      // ساخت یک String برای نمایش، شامل isActive (اگر null نباشد)
+      bool isBold = (item.isBold != null && item.isBold == true);
+      return TextSpan(
+        text: item.text.replaceAll('\\n', '\n'), // اعمال استایل بر اساس status
+        style: TextStyle(
+          color: isBold
+              ? Colors.deepOrange
+              : Theme.of(context).colorScheme.onSurface,
+          fontWeight: isBold ? FontWeight.bold : null,
+          fontSize: isBold ? 17.0 : 17.0,
         ),
-        const VerticalDivider(width: 1),
+      );
+    }).toList();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      // ✅ تغییر به Column برای نمایش بالا و پایین
+      children: [
+        // ردیف اول: متن اصلی
+        Expanded(child: _buildOnlyEnglishLayout(mainTexSegments)),
+        const Divider(height: 20),
+        // ردیف دوم: ترجمه
         Expanded(
-          child: ListView(
-            controller: _persianController,
-            padding: const EdgeInsets.all(16),
-            children: [_buildPersianSection()],
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Align(
+              alignment: AlignmentGeometry.centerRight,
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: RichText(
+                  textAlign: TextAlign.right,
+                  text: TextSpan(children: spans),
+                ),
+              ),
+            ),
           ),
         ),
       ],
     );
   }
 
-  Widget _buildEnglishSection() {
+  Widget _buildEnglishSection(List<MainTextSegment> segments) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -181,5 +261,65 @@ class _TopicDetailScreenState extends ConsumerState<TopicDetailScreen> {
 
   String normalizeText(String text) {
     return text.replaceAll('\\n', '\n');
+  }
+
+  void _showPopup(
+    BuildContext context,
+    String text,
+    String translation,
+    String explanation,
+  ) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            text,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ), //! 💡
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  'ترجمه فارسی:',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                  textDirection: TextDirection.rtl,
+                ),
+                Text(translation, textDirection: TextDirection.rtl),
+                SizedBox(height: 16.0),
+                // const Divider(height: 20),
+                Text(
+                  'توضیحات تکمیلی:',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                  textDirection: TextDirection.rtl,
+                ),
+                Text(explanation, textDirection: TextDirection.rtl),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                'بستن',
+                style: TextStyle(
+                  fontFamily: FontFamily.yekanBakhBold.asText,
+                  fontSize: 16.0,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
