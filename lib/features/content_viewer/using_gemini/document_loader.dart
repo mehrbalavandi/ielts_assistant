652c627506b2428f087104abee72fe86d50d9e48
mehrbalavandi <mehrbalavandi@gmail.com>
2026-07-08 13:46:36 +0330

    Revert "#"
    This reverts commit 314e306b63d107a1c392e5d6e2e19b12144bdc45, reversing
    changes made to faa775db43077f9e90bef14bcfc00aae2085f9c3.

--- lib/features/content_viewer/using_gemini/document_loader.dart
+++ lib/features/content_viewer/using_gemini/document_loader.dart
@@ -1,3 +1,34 @@
+// import 'dart:convert';
+// import 'dart:io';
+// import 'package:flutter/services.dart' show rootBundle;
+// import 'package:ielts_assistant/features/content_viewer/presentation/using_gemini/models.dart';
+
+// class DocumentLoader {
+//   static Future<List<PageData>> loadBookFromJson(String path) async {
+//     String jsonString = '';
+
+//     // 🌟 بررسی هوشمند: فایل در حافظه گوشی است یا دارایی برنامه (assets)؟
+//     if (path.startsWith('assets/')) {
+//       jsonString = await rootBundle.loadString(path);
+//     } else {
+//       final file = File(path);
+//       if (await file.exists()) {
+//         jsonString = await file.readAsString();
+//       } else {
+//         throw Exception("فایل در حافظه گوشی یافت نشد: $path");
+//       }
+//     }
+
+//     List<dynamic> jsonList = jsonDecode(jsonString);
+//     List<PageData> allPages = [];
+
+//     for (var pageJson in jsonList) {
+//       allPages.add(PageData.fromJson(pageJson));
+//     }
+//     return allPages;
+//   }
+// }
+
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
